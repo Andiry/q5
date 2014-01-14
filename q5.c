e0e506454eea3e9c300988e01c81000fac5b09bc
Andiry Xu <jix024@cs.ucsd.edu>
2014-01-13 17:53:06 -0800

    Add time measurement and input format

--- q5.c
+++ q5.c
@@ -15,14 +15,17 @@ void print_array(int **a, int num)
 	}
 }
 
-void format_array(int **a, int num, int prob)
+void format_array(int **a, int num, int prob, int bipar)
 {
 	int i, j;
 
+	for (i = 0; i < num; i++)
+		a[i][i] = 0;
+
 	srand(time(NULL));
 	for (i = 0; i < num; i++) {
-		for (j = i; j < num; j++) {
-			if (i != j && rand() % 100 < prob) {
+		for (j = i + 1; j < num; j++) {
+			if (rand() % 100 < prob) {
 				a[i][j] = 1;
 				a[j][i] = 1;
 			} else {
@@ -68,17 +71,24 @@ found:
 	return result;
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
 	int n, i;
 	int num_nodes;
 	int **a;
 	int result;
 	int prob;
+	int bipar;
+	long long time;
+	struct timespec start, end;
 
-	printf("Please input the number of nodes, in log2:\n");
-	scanf("%d", &n);
 
+	if (argc < 4) {
+		printf("Format: ./q5 $num_nodes_in_log2 $edge_density_in_percentage $if_bipartite\n");
+		return 0;
+	}
+
+	n = atoi(argv[1]);
 	if (n <= 0)
 		return 0;
 
@@ -87,16 +97,23 @@ int main(void)
 	for (i = 0; i < num_nodes; i++)
 		a[i] = malloc(num_nodes * sizeof(int));
 
-	printf("Please input the probability of edge, in percentage:\n");
-	scanf("%d", &prob);
-	format_array(a, num_nodes, prob);
-	print_array(a, num_nodes);
+	prob = atoi(argv[2]);
+	bipar = atoi(argv[3]);
+	format_array(a, num_nodes, prob, bipar);
+//	print_array(a, num_nodes);
+
+	clock_gettime(CLOCK_MONOTONIC, &start);
 	result = check_triangle(a, num_nodes);
+	clock_gettime(CLOCK_MONOTONIC, &end);
+
+	time = (end.tv_sec - start.tv_sec) *1e9 + (end.tv_nsec - start.tv_nsec);
 
 	if (result)
 		printf("There is a triangle in the graph\n");
 	else
 		printf("There is not a triangle in the graph\n");
+
+	printf("Algorithm takes %lld nanoseconds\n", time);
 
 	for (i = 0; i < num_nodes; i++)
 		free(a[i]);
